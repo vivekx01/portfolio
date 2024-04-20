3a42cce10890451e3b60131d9411144cbf8ae50b
Vivek <viveks95944@gmail.com>
2024-04-20 13:27:48 +0530

    workflow_change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/portfolio",
-  distDir: "out", // This sets the output directory to "out"
   output: "export",
   reactStrictMode: true,
 };
